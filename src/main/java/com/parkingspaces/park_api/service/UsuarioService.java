9c894db3da21ff943fcfbd4b634f105ee0b13ea9
Duanderson de Medeiros Block <duandersonblock@gmail.com>
2026-03-20 06:23:55 -0300

    Add updatePassword method to UsuarioController and editarSenha method to UsuarioService

--- src/main/java/com/parkingspaces/park_api/service/UsuarioService.java
+++ src/main/java/com/parkingspaces/park_api/service/UsuarioService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
@@ -30,5 +32,8 @@ public class UsuarioService {
         return user;
     }
 
-
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
 }
