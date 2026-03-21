40246a8e19168e10ce6f15aebe293cf0b8b70e8b
Duanderson de Medeiros Block <duandersonblock@gmail.com>
2026-03-21 07:04:03 -0300

    Update getById method in UsuarioController to return UsuarioResponseDto

--- src/main/java/com/parkingspaces/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/parkingspaces/parkapi/web/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.parkingspaces.parkapi.entity.Usuario;
 import com.parkingspaces.parkapi.service.UsuarioService;
 import com.parkingspaces.parkapi.web.dto.UsuarioCreateDto;
 import com.parkingspaces.parkapi.web.dto.UsuarioResponseDto;
+import com.parkingspaces.parkapi.web.dto.UsuarioSenhaDto;
 import com.parkingspaces.parkapi.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -38,8 +39,8 @@ public class UsuarioController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
-        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
-        return ResponseEntity.ok(user);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+        Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(),dto.getConfirmaSenha());
+        return ResponseEntity.noContent().build();
     }
 }
