06748990f6c3788ddca41ba2d2083765c1d69876
Duanderson de Medeiros Block <duandersonblock@gmail.com>
2026-03-22 11:51:51 -0300

    Add validation annotations to UsuarioSenhaDto and update updatePassword method in UsuarioController

--- src/main/java/com/parkingspaces/parkapi/web/dto/UsuarioSenhaDto.java
+++ src/main/java/com/parkingspaces/parkapi/web/dto/UsuarioSenhaDto.java
@@ -1,5 +1,7 @@
 package com.parkingspaces.parkapi.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +11,13 @@ import lombok.*;
 @ToString
 public class UsuarioSenhaDto {
 
+    @NotBlank
+    @Size(min = 6, max = 6, message = "O campo password deve conter exatamente 6 caracteres")
     private String senhaAtual;
+    @NotBlank
+    @Size(min = 6, max = 6, message = "O campo password deve conter exatamente 6 caracteres")
     private String novaSenha;
+    @NotBlank
+    @Size(min = 6, max = 6, message = "O campo password deve conter exatamente 6 caracteres")
     private String confirmaSenha;
 }
