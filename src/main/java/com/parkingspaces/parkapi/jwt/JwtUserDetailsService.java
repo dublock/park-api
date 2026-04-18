c6e0695715650541d792c97f82a37f77d01f2742
Duanderson de Medeiros Block <duandersonblock@gmail.com>
2026-04-18 06:46:34 -0300

    Implement JWT user details service and update Usuario repository for username-based queries

---
+++ src/main/java/com/parkingspaces/parkapi/jwt/JwtUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.parkingspaces.parkapi.jwt;
+
+import com.parkingspaces.parkapi.entity.Usuario;
+import com.parkingspaces.parkapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService usuarioService;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.buscarPorUsername(username);
+        return new JwtUserDetails(usuario);
+    }
+
+    public JwtToken getTokenAuthentication(String username) {
+        Usuario.Role role = usuarioService.buscarRolePorUsername(username);
+        return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
+    }
+}
--- src/main/java/com/parkingspaces/parkapi/repository/UsuarioRepository.java
+++ src/main/java/com/parkingspaces/parkapi/repository/UsuarioRepository.java
@@ -2,9 +2,15 @@ package com.parkingspaces.parkapi.repository;
 
 import com.parkingspaces.parkapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 
+   Optional <Usuario>  findByUsername(String username);
 
+   @Query("select u.role from Usuario u where u.username like :username")
+   Usuario.Role findRoleByUserName(String username);
 }
